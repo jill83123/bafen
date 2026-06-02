5028bfe36f0402c8aa91c0416c8d9d88e66ecbdd
YU-FANG <jill83123@gmail.com>
2026-06-02 18:12:27 +0800

    feat: 完成後台的編輯標籤、刪除指定標籤 API

---
+++ server/api/admin/tags/[id].delete.ts
@@ -0,0 +1,30 @@
+import { tagTable } from '#server/db/schema';
+import { eq } from 'drizzle-orm';
+import { z } from 'zod';
+
+const ParamSchema = z.object({
+  id: z
+    .string()
+    .regex(/^\d*$/)
+    .transform((value) => Number(value)),
+});
+
+export default defineEventHandler(async (event) => {
+  const param = getRouterParams(event);
+  const paramParseResult = ParamSchema.safeParse(param);
+  if (!paramParseResult.success) throw createError({ statusCode: 400 });
+
+  const { id: tagId } = paramParseResult.data;
+
+  // 檢查標籤是否存在
+  const [existingTagById] = await db
+    .select({ id: tagTable.id })
+    .from(tagTable)
+    .where(eq(tagTable.id, tagId));
+
+  if (!existingTagById) throw createError({ statusCode: 400, message: '標籤不存在' });
+
+  await db.delete(tagTable).where(eq(tagTable.id, tagId));
+
+  return {};
+});
