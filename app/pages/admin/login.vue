b08bd1886da4d1a5c214813093ef90e48982d99f
YU-FANG <jill83123@gmail.com>
2026-06-06 01:46:52 +0800

    feat: 完成登入頁

--- app/pages/admin/login.vue
+++ app/pages/admin/login.vue
@@ -1,25 +1,60 @@
 <template>
-  <div>
-    <button type="button" class="flex items-center" @click="handleLogin">
-      <Icon name="material-icon-theme:google" />使用 Google 繼續
-    </button>
-  </div>
+  <main class="h-screen">
+    <div class="flex h-full flex-col items-center justify-center p-3">
+      <UPageCard class="w-full max-w-md p-1">
+        <UAuthForm
+          icon="i-ri-admin-line"
+          title="八分室內裝修｜後台登入"
+          :providers="providers"
+          :ui="{ title: 'font-medium' }"
+        >
+          <template #footer>
+            <NuxtLink to="/" class="text-brand-main hover:text-brand-hover -my-2 p-2 font-medium">
+              回首頁
+            </NuxtLink>
+          </template>
+        </UAuthForm>
+      </UPageCard>
+
+      <p v-show="errorMessage" class="text-error mt-7 flex items-center gap-1">
+        <Icon name="i-material-symbols-error-outline" />{{ errorMessage }}
+      </p>
+    </div>
+  </main>
 </template>
 
 <script lang="ts" setup>
 import { googleTokenLogin } from 'vue3-google-login';
-import { FetchError } from 'ofetch';
+import type { ButtonProps } from '@nuxt/ui';
 
 const runtimeConfig = useRuntimeConfig();
 const { googleClientId: GOOGLE_CLIENT_ID } = runtimeConfig.public;
 
 const toast = useAppToast();
 
+const providers = ref<ButtonProps[]>([
+  {
+    label: '使用 Google 繼續',
+    icon: 'material-icon-theme:google',
+    color: 'neutral',
+    variant: 'soft',
+    ui: { base: 'gap-2' },
+    onClick: () => {
+      handleLogin();
+    },
+  },
+]);
+
+const errorMessage = ref('');
+
 const handleLogin = async () => {
+  errorMessage.value = '';
+
   const accessToken = await googleTokenLogin({ clientId: GOOGLE_CLIENT_ID })
     .then((response) => response.access_token)
     .catch((error) => {
-      if (!error.type?.includes('popup_closed')) toast.error('Google 登入失敗');
+      const isPopupClosed = error.type?.includes('popup_closed');
+      if (!isPopupClosed) toast.error('Google 登入失敗，請稍後再試');
       return null;
     });
 
@@ -33,8 +68,7 @@ const handleLogin = async () => {
     toast.success('登入成功');
     navigateTo('/admin', { replace: true });
   } catch (error) {
-    toast.error(getErrorMessage(error, '登入失敗'));
-    if (error instanceof FetchError && error.status === 403) navigateTo('/', { replace: true });
+    errorMessage.value = getErrorMessage(error, '登入失敗，請稍後再試');
   }
 };
 </script>
